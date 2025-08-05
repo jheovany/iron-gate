69776b1dcc5fa4b831c23e4e1f99a8e41adefc79
jheovany <jheovany.menjivarg@gmail.com>
2025-08-05 00:06:16 -0600

    Add TODO comment to verify functionality of AdminController

--- src/main/kotlin/com/bluelock/irongate/controller/AdminController.kt
+++ src/main/kotlin/com/bluelock/irongate/controller/AdminController.kt
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+// TODO: Verify total functionality of this controller
 @RestController
 @RequestMapping("/api/v1/admin")
 class AdminController(
