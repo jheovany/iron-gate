92832529439f7add5e817217dc4628593866d4fe
jheovany <jheovany.menjivarg@gmail.com>
2025-08-07 23:15:18 -0600

    Version is specified in 'version.properties'

--- src/main/kotlin/com/bluelock/irongate/controller/AdminController.kt
+++ src/main/kotlin/com/bluelock/irongate/controller/AdminController.kt
@@ -7,13 +7,7 @@ import com.bluelock.irongate.service.UserService
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 // TODO: Verify total functionality of this controller
 @RestController
